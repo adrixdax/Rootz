c4a306e3821da7d815cdf40ff0c07a4aa06707c3
Adriano <adrianodalessandro@yahoo.it>
2024-02-27 17:15:18 +0100

    Initial Commit

---
+++ Rootz/RootzApp.swift
@@ -0,0 +1,17 @@
+//
+//  RootzApp.swift
+//  Rootz
+//
+//  Created by Adriano d'Alessandro on 27/02/24.
+//
+
+import SwiftUI
+
+@main
+struct RootzApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
